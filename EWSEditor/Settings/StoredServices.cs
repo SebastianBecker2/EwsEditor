baafff8d1fd9f03821599dc4d6056c5d86cb6105
Sebastian Becker <s.becker@comundus.com>
2018-07-17 12:39:09 +0200

    Fix deserialization of stored services
    The deserialization process can't create new instances of the network credentials. Thus
    it reused the same instance of the network credentials. (Still not 100% why this happens)
    This is now fixed by deserializing each stored services individually and copying the
    network credentials of each stored service manually.

--- EWSEditor/Settings/StoredServices.cs
+++ EWSEditor/Settings/StoredServices.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.Exchange.WebServices.Data;
 using EWSEditor.Common;
 using Newtonsoft.Json;
+using System.Net;
 
 namespace EWSEditor.Settings
 {
@@ -30,13 +31,44 @@ namespace EWSEditor.Settings
 
         public void Deserialize(string json)
         {
-            var services = JsonConvert.DeserializeObject<StoredServices>(json, new JsonSerializerSettings
-            {
-                TypeNameHandling = TypeNameHandling.Auto,
-                NullValueHandling = NullValueHandling.Ignore,
-            });
             this.Clear();
-            this.AddRange(services);
+
+            string json_part = "";
+            var brace_counter = 0;
+            foreach (var ch in json)
+            {
+                if (ch == '{')
+                {
+                    brace_counter++;
+                }
+
+                if (brace_counter >= 1)
+                {
+                    json_part += ch;
+                }
+
+                if (ch == '}')
+                {
+                    brace_counter--;
+                }
+
+                if ((brace_counter == 0) && (!string.IsNullOrWhiteSpace(json_part)))
+                {
+                    var service = JsonConvert.DeserializeObject<StoredService>(json_part, new JsonSerializerSettings
+                    {
+                        TypeNameHandling = TypeNameHandling.Auto,
+                        NullValueHandling = NullValueHandling.Ignore,
+                    });
+
+                    var web_cred = service.Service.Credentials as WebCredentials;
+                    var net_cred = web_cred.Credentials as NetworkCredential;
+                    service.Service.Credentials = new NetworkCredential(net_cred.UserName, net_cred.Password, net_cred.Domain);
+
+                    this.Add(service);
+
+                    json_part = "";
+                }
+            }
         }
     }
 }
